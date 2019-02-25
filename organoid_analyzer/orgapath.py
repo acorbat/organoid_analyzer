3480a6b2edd20da13c2c6bb41e5e23dd2f207d71
acorbat <acorbat@df.uba.ar>
2019-02-25 14:17:17 +0100

    orgapath add_crop now looks for suffix and doesn't duplicate it.

--- organoid_analyzer/orgapath.py
+++ organoid_analyzer/orgapath.py
@@ -242,5 +242,5 @@ def add_crop_to_yaml(filename, crop_filename=None):
     except KeyboardInterrupt:
         pass
 
-    with open(filename + '_crop.yaml', 'w', encoding='utf-8') as fo:
+    with open(filename[:filename.rfind('.')] + '_crop.yaml', 'w', encoding='utf-8') as fo:
         fo.write(yaml.dump(dout))
