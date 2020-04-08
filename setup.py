50cc4b1061e730db2980d9c170f96807cd8c81a7
acorbat <acorbat@df.uba.ar>
2020-04-08 10:44:08 -0300

    Added a setup file for instalation

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='organoid_analyzer',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/acorbat/organoid_analyzer/tree/master/',
+    license='MIT',
+    author='Agustin Corbat',
+    author_email='acorbat@df.uba.ar',
+    description='Package used for analysing homoFRET experiments with caspase '
+                'biosensors.',
+    install_requires=['imageio', 'IPython', 'matplotlib', 'numpy', 'pandas',
+                      'mahotas', 'pyyaml', 'scikit-image', 'scikit-learn',
+                      'scipy', 'seaborn',
+                      'img_manager @ git+https://github.com/acorbat/img_manager.git',
+                      'serialize @ git+https://github.com/hgrecco/serialize.git']
+)
